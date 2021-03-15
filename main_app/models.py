a02add27caab8d30cd729be6640432587b965af2
carmenlop <cllopez92@gmail.com>
2021-03-15 15:20:43 -0700

    added user and profile models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    current_city = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
