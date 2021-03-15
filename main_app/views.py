5d7860d1c804af4aea85bcc07a10259d2e50c48e
carmenlop <cllopez92@gmail.com>
2021-03-15 15:34:51 -0700

    added signup view

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 # Define the home view
 def home(request):
@@ -8,5 +11,19 @@ def home(request):
 # Define the home view
 def about(request):
   return render(request, 'about.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return HttpResponse('working signup')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message }
+  return render(request, 'registration/signup.html', context)
 
 
