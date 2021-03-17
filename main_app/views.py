5f880026190bb7b42eaf96c5452a7b9797e506c5
skaiser33 <skaiser33@gmail.com>
2021-03-17 18:53:28 -0400

    adds profile display for users with no reviews

--- main_app/views.py
+++ main_app/views.py
@@ -60,29 +60,28 @@ def profiles_edit(request, profile_id):
     return render(request, 'profile/edit.html', { 'profile': profile, 'profile_form': profile_form })
 
 # Define the reviews index view
-@login_required
+# @login_required
 def reviews_index(request):
   profile = Profile.objects.get(user=request.user)
   reviews = Review.objects.filter(profile_id=profile.id)
   return render(request, 'reviews/user_reviews/index.html', { 'reviews': reviews})    
 
 # Define the reviews detail view
-@login_required
+# @login_required
 def reviews_detail(request, review_id):
   review = Review.objects.get(id=review_id)
   profile = Profile.objects.get(id=review.profile_id)
-  return render(request, 'user_reviews/detail.html', {'review':review, 'profile':profile })
+  return render(request, 'reviews/user_reviews/detail.html', {'review':review, 'profile':profile })
 
 def reviews_new(request, city_id):
   review_form = ReviewForm(request.POST or None)
   profile = Profile.objects.get(user=request.user)
-  city = City.objects.get(id=city_id)
   if request.POST and review_form.is_valid():
     new_review = review_form.save(commit=False)
-    new_review.city_id = city.id
+    new_review.city_id = city_id
     new_review.profile_id = profile.id
     new_review.save()
-    return redirect(request, 'cities_detail', city_id=city.id)
+    return redirect(request, 'cities_detail', city_id=city_id)
   else:
     return render(request, 'reviews/new.html', {'review_form':review_form})
 
