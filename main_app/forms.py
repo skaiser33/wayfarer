6cae342edeb1f42c2138ee7021d6efc5c8489873
skaiser33 <skaiser33@gmail.com>
2021-03-18 15:00:08 -0400

    adds crispy forms and styling tweaks

--- main_app/forms.py
+++ main_app/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile, Review
+# from crispy_forms.helper import FormHelper
 
 class ProfileForm(forms.ModelForm):
   first_name = forms.CharField(max_length=100)
@@ -14,7 +15,8 @@ class ProfileForm(forms.ModelForm):
 
 class ReviewForm(forms.ModelForm):
   title = forms.CharField(max_length=200, required=True)
-  content = forms.CharField(required=True)
+  content = forms.CharField(required=True, label="Content",
+                                widget=forms.Textarea())
   
   class Meta:
     model = Review
