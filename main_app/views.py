406ab960ba1a9311729465a2dfa88ebc44772452
skaiser33 <skaiser33@gmail.com>
2021-03-18 17:22:50 -0400

    adds home layout and responsive design features

--- main_app/views.py
+++ main_app/views.py
@@ -49,7 +49,7 @@ def profile_setup(request):
     return render(request, 'profile_setup.html', { 'profile_form': profile_form })
 
 # Define the view to edit profile
-@login_required
+# @login_required
 def profiles_edit(request, profile_id):
   profile = Profile.objects.get(id=profile_id)
   profile_form = ProfileForm(request.POST or None, instance=profile)
@@ -101,7 +101,7 @@ def reviews_edit(request, review_id):
 # @login_required
 def reviews_delete(request, review_id):
   Review.objects.get(id=review_id).delete()
-  return redirect('index')         
+  return redirect('profile')         
 
 #Define the cities index view
 def cities_index(request):
